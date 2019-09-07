9201c544eac56ad3464adca535075cfa7fa5f1bc
Sivin <xiwenhec@163.com>
2019-09-07 15:58:34 +0800

    优化Muxer架构，完善Mp4Muxer类

--- EffectLib/src/main/java/com/gpufast/recorder/muxer/MediaMuxer.java
+++
@@ -1,9 +0,0 @@
-package com.gpufast.recorder.muxer;
-
-import com.gpufast.recorder.video.VideoEncoder;
-
-/**
- * 视频复用器
- */
-public abstract class MediaMuxer implements VideoEncoder.VideoEncoderCallback {
-}
--- EffectLib/src/main/java/com/gpufast/recorder/muxer/MediaMuxerFactory.java
+++ EffectLib/src/main/java/com/gpufast/recorder/muxer/MediaMuxerFactory.java
@@ -1,13 +1,19 @@
 package com.gpufast.recorder.muxer;
 
+import com.gpufast.recorder.RecordParams;
+
 public class MediaMuxerFactory {
 
-
-    public MediaMuxer createMediaMuxer() {
-
-
+    public static IMediaMuxer createMediaMuxer(RecordParams params, MuxerType type) {
+        switch (type) {
+            case MP4:
+                return Mp4MuxerCreator.create(params);
+            case FLV:
+            case MKV:
+            case AVI:
+                return null;
+        }
         return null;
-
     }
 
 
