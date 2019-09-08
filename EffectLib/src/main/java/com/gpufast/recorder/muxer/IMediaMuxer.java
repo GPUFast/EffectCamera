f29d29f356245238ec385addeb74182b267fc577
Sivin <xiwenhec@163.com>
2019-09-08 17:41:17 +0800

    解决合成MP4丢失关键帧，引起无法录制的问题

--- EffectLib/src/main/java/com/gpufast/recorder/muxer/IMediaMuxer.java
+++ EffectLib/src/main/java/com/gpufast/recorder/muxer/IMediaMuxer.java
@@ -1,23 +1,27 @@
 package com.gpufast.recorder.muxer;
 
+
 import com.gpufast.recorder.audio.encoder.AudioEncoder;
 import com.gpufast.recorder.video.VideoEncoder;
+
 
 /**
  * 视频复用器
  */
-public abstract class IMediaMuxer implements VideoEncoder.VideoEncoderCallback, AudioEncoder.AudioEncoderCallback {
+public abstract class IMediaMuxer implements VideoEncoder.VideoEncoderCallback,
+        AudioEncoder.AudioEncoderCallback {
+
     static class Setting {
         String savePath;
-        boolean enableVideo;
-        boolean enableAudio;
+        boolean muteMic;
 
-        Setting(String savePath, boolean enableVideo, boolean enableAudio) {
+        Setting(String savePath, boolean muteMic) {
             this.savePath = savePath;
-            this.enableVideo = enableVideo;
-            this.enableAudio = enableAudio;
+            this.muteMic = muteMic;
         }
     }
 
-    public abstract void release();
+    abstract void release();
+
+
 }
